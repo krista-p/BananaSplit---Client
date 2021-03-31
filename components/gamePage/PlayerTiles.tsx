51c10dcfe2b871f62c908346180c421e1d10c313
Nick Villarreal <nick33v@gmail.com>
2021-03-30 20:24:05 -0400

    feat: merge with feat/gameEndPopup

--- components/gamePage/PlayerTiles.tsx
+++ components/gamePage/PlayerTiles.tsx
@@ -6,32 +6,6 @@ import Tile from './Tile';
 
 export default function PlayerTiles({ state }) {
   return (
-<<<<<<< HEAD
-    <div className="w-full h-full flex justify-center bg-secondary rounded-full mt-4 px-8 overflow-x-scroll scroll-bar-light">
-      <Droppable
-        droppableId="playerTiles"
-        direction="horizontal"
-      >
-        {(provided) => (
-          <div
-            className="w-full min-w-max flex items-center"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {
-              state.playerTiles.map((currentTile: TileInterface, index: number) => (
-                <Tile
-                  key={currentTile.id}
-                  currentTile={currentTile}
-                  index={index}
-                />
-              ))
-            }
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-=======
     <div className="w-full h-4/5 flex justify-center bg-secondary rounded-full mt-4 px-8">
       <div className="w-4/5 h-full overflow-x-scroll overflow-y-hidden scroll-bar-light scrollbar-track-gray-100 m-4">
         <Droppable
@@ -58,7 +32,6 @@ export default function PlayerTiles({ state }) {
           )}
         </Droppable>
       </div>
->>>>>>> d07a16f2785a8b84850fab3a785747d6f9bd96b9
     </div>
   );
 }
