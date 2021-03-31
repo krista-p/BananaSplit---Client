378b25aa6a73762ba11877b345d76116acc09f86
Krista Polikaitis <kristabrak31@gmail.com>
2021-03-31 18:11:56 -0500

    fix: merge my errors

--- components/gamePage/gameEndPopup/EndStats.tsx
+++ components/gamePage/gameEndPopup/EndStats.tsx
@@ -1,5 +1,5 @@
 const EndStats = ({ rottenBanana, endGameLongestWord, endGameLongestWordUser, endGameMostWords, endGameMostWordUsers }) => (
-  <div className="text-primary text-2xl w-full bg-secondary rounded-2xl p-6 text-center mr-2">
+  <div className="text-primary text-2xl w-full bg-secondary rounded-2xl p-6 text-center mr-2 mt-28">
     <div className="mb-2 text-lg md:text-2xl">
       <h1 className="text-lg md:text-3xl p-2 bg-primary text-secondary rounded-full mb-2">longest word</h1>
       <h2>{endGameLongestWordUser}</h2>
@@ -18,7 +18,7 @@ const EndStats = ({ rottenBanana, endGameLongestWord, endGameLongestWordUser, en
     {/* only show if there is a rotton banana */}
     <div className="mb-2 text-lg md:text-2xl">
       <h1 className="text-lg md:text-3xl p-2 bg-primary text-secondary rounded-full mb-2">rotten banana</h1>
-      <h2>{rottenBanana}</h2>
+      <h2>{rottenBanana ? rottenBanana : 'no rotten bananas!'}</h2>
     </div>
   </div>
 );
